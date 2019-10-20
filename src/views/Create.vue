3266bdc0ffb257f11a37fae102af05be2a2b81c6
David <D.Barringer@warwick.ac.uk>
2019-10-20 18:00:47 +0100

    Stage 3, adding and updating items

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,21 +3,26 @@
     <h1>Create a new list</h1>
     <ul>
       <li v-for="(item,index) in list" :key="index">
-        {{item.itemName}}
-        ({{item.quantity}})
+        <input type="text" v-model="list[index].itemName">
+        <input type="number" v-model="list[index].quantity">
       </li>
     </ul>
+    <button @click="newItem()">+</button>
   </div>
 </template>
-
 <script>
   export default{
     data(){
       return{
         list: [
-          {itemName:"Bread", quantity: 1, acquired: false}
+          {itemName:"", quantity: 0, acquired: false}
         ]
       }
+    },
+    methods:{
+      newItem(){
+        this.list.push({itemName:"", quantity:0, acquired:false});
+      },
     }
   }
 </script>
