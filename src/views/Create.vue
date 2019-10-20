13a77c5705a4c7d8bcbd8998b08258cd25a4c04b
David <D.Barringer@warwick.ac.uk>
2019-10-20 17:59:38 +0100

    Stage 2, viewing items

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="create">
     <h1>Create a new list</h1>
+    <ul>
+      <li v-for="(item,index) in list" :key="index">
+        {{item.itemName}}
+        ({{item.quantity}})
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return{
+        list: [
+          {itemName:"Bread", quantity: 1, acquired: false}
+        ]
+      }
+    }
+  }
+</script>
